699adc2dade723c001472fe13fd400f413fc27f8
Mark Fehrenbacher <kendopunk@hotmail.com>
2022-05-16 15:12:37 -0400

    Testing out original Jira implementation

--- src/actions/jira/jira.ts
+++ src/actions/jira/jira.ts
@@ -1,6 +1,149 @@
 /**
  * src/actions/jira/jira.ts
  */
+ import * as Hub from "../../hub"
+
+ import { URL } from "url"
+
+ const jiraApi = require("jira-client")
+
+ const apiVersion = "2"
+
+ export class JiraAction extends Hub.Action {
+
+   name = "jira_create_issue"
+   label = "JIRA"
+   iconName = "jira/jira.svg"
+   description = "Create a JIRA issue referencing data."
+   params = [
+     {
+       description: "The address of your JIRA server ex. https://myjira.atlassian.net.",
+       label: "Address",
+       name: "address",
+       required: true,
+       sensitive: false,
+     }, {
+       description: "The JIRA username assigned to create issues for Looker.",
+       label: "Username",
+       name: "username",
+       required: true,
+       sensitive: false,
+     }, {
+       description: "The password for the JIRA user assigned to Looker.",
+       label: "Password",
+       name: "password",
+       required: true,
+       sensitive: true,
+     },
+   ]
+   supportedActionTypes = [Hub.ActionType.Query]
+   requiredFields = []
+
+   async execute(request: Hub.ActionRequest) {
+     if (!request.attachment || !request.attachment.dataBuffer) {
+       throw "Couldn't get data from attachment"
+     }
+
+     const jira = this.jiraClientFromRequest(request)
+
+     const issue = {
+       fields: {
+         project: {
+           id: request.formParams.project,
+         },
+         summary: request.formParams.summary,
+         description: `${request.formParams.description}` +
+           `\nLooker URL: ${request.scheduledPlan && request.scheduledPlan.url}`,
+         issuetype: {
+           id: request.formParams.issueType,
+         },
+       },
+     }
+     let response
+     try {
+       await jira.addNewIssue(issue)
+     } catch (e) {
+       if (e instanceof Error) {
+        response = {success: false, message: e.message}
+       } else {
+        response = {success: false, message: "Error"}
+       }
+     }
+     return new Hub.ActionResponse(response)
+   }
+
+   async form(request: Hub.ActionRequest) {
+
+     const form = new Hub.ActionForm()
+     try {
+       const jira = this.jiraClientFromRequest(request)
+
+       const [projects, issueTypes] = await Promise.all([
+         jira.listProjects(),
+         jira.listIssueTypes(),
+       ])
+
+       form.fields = [{
+         default: projects[0].id,
+         label: "Project",
+         name: "project",
+         options: projects.map((p: any) => {
+           return {name: p.id, label: p.name}
+         }),
+         type: "select",
+         required: true,
+       }, {
+         label: "Summary",
+         name: "summary",
+         type: "string",
+         required: true,
+       }, {
+         label: "Description",
+         name: "description",
+         type: "textarea",
+         required: true,
+       }, {
+         default: issueTypes[0].id,
+         label: "Issue Type",
+         name: "issueType",
+         type: "select",
+         options: issueTypes
+           .filter((i: any) => i.description)
+           .map((p: any) => {
+             return {name: p.id, label: p.name}
+           }),
+         required: true,
+       }]
+     } catch (e) {
+       if (e instanceof Error) {
+        form.error = e
+       } else {
+        form.error = new Error("Error")
+       }
+     }
+     return form
+   }
+
+   private jiraClientFromRequest(request: Hub.ActionRequest) {
+     const parsedUrl = new URL(request.params.address!)
+     if (!parsedUrl.host) {
+       throw "Invalid JIRA server address."
+     }
+     return new jiraApi({
+       protocol: parsedUrl.protocol ? parsedUrl.protocol : "https",
+       host: parsedUrl.host,
+       port: parsedUrl.port ? parsedUrl.port : "443",
+       username: request.params.username,
+       password: request.params.password,
+       apiVersion,
+     })
+   }
+
+ }
+
+ Hub.addAction(new JiraAction())
+
+/*
 import * as winston from "winston"
 
 import * as Hub from "../../hub"
@@ -36,7 +179,6 @@ export class JiraAction extends Hub.OAuthAction {
     return resp
   }
 
-  /*
   // async execute(request: Hub.ActionRequest) {
   //   const filename = this.dropboxFilename(request)
   //   const directory = request.formParams.directory
@@ -67,7 +209,7 @@ export class JiraAction extends Hub.OAuthAction {
   //     resp.message = "No data sent from Looker to be sent to Dropbox."
   //   }
   //   return resp
-  // }*/
+  // }
 
   async oauthCheck(_request: Hub.ActionRequest): Promise<boolean> {
     throw new Error("Method not implemented.")
@@ -82,13 +224,14 @@ export class JiraAction extends Hub.OAuthAction {
   }
 }
 
-/******** Register with Hub if prereqs are satisfied ********/
+// ******* Register with Hub if prereqs are satisfied ********
 if (process.env.JIRA_CLIENT_ID && process.env.JIRA_CLIENT_SECRET) {
   const jha = new JiraAction(process.env.JIRA_CLIENT_ID, process.env.JIRA_CLIENT_SECRET)
   Hub.addAction(jha)
 } else {
   winston.warn("Jira Issue Action not registered because required environment variables are missing.")
 }
+*/
 
 /*
 import * as Hub from "../../hub"
